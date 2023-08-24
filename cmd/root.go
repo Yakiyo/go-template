37c070e41178e9261243044d80d452cd480a33bd
Yakiyo <yakiyo1206@gmail.com>
2023-08-24 11:28:01 +0000

    feat: config handling

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/Yakiyo/go-template/config"
+	logger "github.com/Yakiyo/go-template/log"
 	"github.com/Yakiyo/go-template/meta"
 	cc "github.com/ivanpirog/coloredcobra"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var rootCmd = &cobra.Command{
@@ -14,7 +18,10 @@ var rootCmd = &cobra.Command{
 	Long:    `Longer description`,
 	Version: meta.Version,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-
+		config.BindFlags(cmd)
+		logger.SetLevel(viper.GetString("log_level"))
+		fmt.Println("In pre-run")
+		fmt.Printf("%#v\n", viper.AllSettings())
 		return nil
 	},
 }
@@ -38,6 +45,10 @@ func init() {
 		NoExtraNewlines: true,
 	})
 
+	f := rootCmd.PersistentFlags()
 	// allow users to set custom config files
-	rootCmd.PersistentFlags().StringP("config", "c", "", "Path to config file")
+	f.StringP("config", "c", "", "Path to config file")
+	// dont mention debug level, usually users dont need, only on the dev side
+	f.String("log-level", "", "Set log level [info, warn, error, fatal]")
+	f.String("color", "", "Set color output [always, auto, never]")
 }
